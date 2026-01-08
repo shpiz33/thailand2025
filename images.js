2ed6adc8ad707081b685339fd9847aaafd359e39
shpiz33 <81231047+shpiz33@users.noreply.github.com>
2026-01-08 23:57:39 +0800

    Add files via upload

---
+++ images.js
@@ -0,0 +1,8 @@
+const SCENE_IMAGES = {
+  1: ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'],      // Scene 1 floating
+  2: Array.from({length:20}, (_,i)=>`${i+1}.jpg`),
+  3: Array.from({length:20}, (_,i)=>`${i+1}.jpg`),
+  4: Array.from({length:20}, (_,i)=>`${i+1}.jpg`),
+  5: Array.from({length:20}, (_,i)=>`${i+1}.jpg`),
+  6: ['video.mp4']
+};
